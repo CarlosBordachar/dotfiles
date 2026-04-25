5e5638c943a539fc67f7d0be02b7f1039ba6c9dd
CarlosBordachar <git@carlosbordachar.com>
2026-04-25 18:11:40 -0300

    feat(scripts): update powermenu custom wlogout
    Define script for custom wlogout invoke. If the script does not exists,
    execute `wlogout` plain.

--- scripts/.config/scripts/powermenu.sh
+++ scripts/.config/scripts/powermenu.sh
@@ -9,7 +9,13 @@ HAS_FUZZEL=0
 command -v fuzzel >/dev/null 2>&1 && HAS_FUZZEL=1
 
 if [ $HAS_WLOGOUT = 1 ]; then
-    wlogout
+    # execute wlogout.sh script if exists (custom invoke) or wlogout plain.
+    #if [ -f ~/.config/wlogout/wlogout.sh ]; then
+    #    . ~/.config/wlogout/wlogout.sh
+    #else
+    #    wlogout
+    #fi
+    [ -f ~/.config/wlogout/wlogout.sh ] && . ~/.config/wlogout/wlogout.sh || wlogout
 elif [ $HAS_FUZZEL = 1 ]; then
     # source ./fuzzel-powermenu.sh
     # . ${0%/*}/fuzzel-powermenu.sh
---
+++ wlogout/.config/wlogout/wlogout.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# Invoke wlogout with custom properties.
+
+wlogout -b 4 -m 400
